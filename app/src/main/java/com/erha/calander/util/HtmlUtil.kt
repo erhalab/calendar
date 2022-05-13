9cb312afa264d57f2f8449625fe438cbb4b5d696
二哈 <yhuang443@gmail.com>
2022-05-13 08:06:10 +0800

    Fix: 无效的正则替换

--- app/src/main/java/com/erha/calander/util/HtmlUtil.kt
+++ app/src/main/java/com/erha/calander/util/HtmlUtil.kt
@@ -2,17 +2,26 @@ package com.erha.calander.util
 
 object HtmlUtil {
     fun convertHtmlToOnlyText(html: String): String {
-        var s = html.replace("<.{1,10}?>", "")
+        var s = html.replace(Regex("<.{1,10}?>"), "")
         s = s.trim()
         s = s.replace("&nbsp;", "")
         s = s.replace(
-            "<input type=\\\"checkbox\\\" name=\\\"[0-9]{12,14}\\\" value=\\\"[0-9]{12,14}\\\">",
+            Regex("<input type=\"checkbox\" name=\"[0-9]{12,14}\" value=\"[0-9]{12,14}\">"),
             ""
         )
         s = s.replace(
-            "<img src=\\\".{0,200}?\\\" alt=\\\".{0,30}\\\" width=\\\"[0-9]{0,5}\\\">",
+            Regex("<img src=\".{0,200}?\" alt=\".{0,30}\" width=\"[0-9]{0,5}\">"),
             ""
         )
         return s
+    }
+
+    fun getHtmlFirstLineOnlyText(html: String): String {
+        var i = html.indexOf("<br>")
+        return if (i == -1) {
+            convertHtmlToOnlyText(html)
+        } else {
+            convertHtmlToOnlyText(html.substring(0, i))
+        }
     }
 }
